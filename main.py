81859554d36a82288bc9ee3ec453bceffad0db75
Alisha Thompson <thompsonac542@gmail.com>
2025-04-22 17:49:32 -0400

    Step 4: Initial Training: Updated main.py file with data_split step

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import json
 
 import mlflow
 import tempfile
@@ -78,10 +79,17 @@ def go(config: DictConfig):
             )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                #version="main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config['modeling']['test_size'],
+                    "random_seed": config['modeling']['random_seed'],
+                    "stratify_by": config['modeling']['stratify_by']
+                }
+            )
 
         if "train_random_forest" in active_steps:
 
